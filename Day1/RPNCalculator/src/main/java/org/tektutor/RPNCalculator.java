81f9f748e99dd16d12298577378155fd3f1bfc05
Jeganathan Swaminathan <mail2jegan@gmail.com>
2019-01-28 21:47:48 -0800

    Added RPN source code as it got remove accidently.

---
+++ Day1/RPNCalculator/src/main/java/org/tektutor/RPNCalculator.java
@@ -0,0 +1,73 @@
+package org.tektutor;
+
+import java.util.Stack;
+
+public class RPNCalculator {
+
+	private Stack<Double> numberStack;
+	private double firstNumber, secondNumber, result;
+
+	public RPNCalculator() {
+		numberStack = new Stack<Double>();
+		firstNumber = secondNumber = result = 0.0;
+	}
+
+	private void retrieveOperands() {
+		secondNumber = numberStack.pop();
+		firstNumber = numberStack.pop();
+	}
+
+	private void add() {
+		retrieveOperands();
+		result = firstNumber + secondNumber;
+		numberStack.push ( result );
+	}
+
+	private void subtract() {
+		retrieveOperands();
+		result = firstNumber - secondNumber;
+		numberStack.push ( result );
+	}
+
+	private void multiply() {
+		retrieveOperands();
+		result = firstNumber * secondNumber;
+		numberStack.push ( result );
+	}
+
+	private void divide() {
+		retrieveOperands();
+		result = firstNumber / secondNumber;
+		numberStack.push ( result );
+	}
+
+	private boolean isMathOperator( String token ) {
+		String strMathOperators = "+-*/";
+		return strMathOperators.contains ( token );
+	}
+
+	public double evaluate ( String rpnMathExpression ) {
+
+		String[] rpnTokens = rpnMathExpression.split ( " " );
+
+		MathOperation mathOperation = null;
+
+		for ( String token : rpnTokens ) {
+			if ( isMathOperator ( token ) ) {
+				retrieveOperands();
+
+				mathOperation = MathFactory.getMathObject ( token );
+				result = mathOperation.evaluate ( firstNumber, secondNumber ); 
+
+				numberStack.push ( result );
+			}
+			else {
+				numberStack.push ( Double.parseDouble ( token ) );
+			}
+		}
+
+		return numberStack.pop();
+	
+	}
+
+}
